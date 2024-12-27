f86da355ed6a7a3c94c283127abfd5ef499af203
Abdullah Alhasan <abdallahalhasan09@gmail.com>
2024-12-27 17:06:40 +0300

    Update stables.js

--- client/src/constants/stables.js
+++ client/src/constants/stables.js
@@ -1,6 +1,6 @@
 const UPLOAD_FOLDER_BASE_URL = `http://localhost:5000/uploads/`;
 const defaultImageBlog = "https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-1080x675.jpg"
-const API_BASE_URL = "http://localhost:5000/api/";
+const API_BASE_URL = "https://dalil-tawny.vercel.app/";
 
 const stables = { UPLOAD_FOLDER_BASE_URL, defaultImageBlog , API_BASE_URL };
 
