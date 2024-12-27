348a9db5696a108621764cbab57c73b2261aa29f
Abdullah Alhasan <abdallahalhasan09@gmail.com>
2024-12-27 17:15:08 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -37,7 +37,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
     cors({
-        origin: "https://rakwa-theta.vercel.app/",
+        origin: "https://dalil-frontend.vercel.app/",
         methods: ["GET", "POST", "DELETE", "PUT"],
         allowedHeaders: [
             "Content-Type",
